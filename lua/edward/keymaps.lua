0b6c18d78e69374bb203f48643791fed03941d1d
EdwardKerckhof <edward@zonedev.be>
2022-10-03 14:32:43 +0200

    init: setup

---
+++ lua/edward/keymaps.lua
@@ -0,0 +1,118 @@
+M = {}
+lvim.leader = "space"
+
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+-- For the description on keymaps, I have a function getOptions(desc) which returns noremap=true, silent=true and desc=desc. Then call: keymap(mode, keymap, command, getOptions("some randome desc")
+
+local keymap = vim.keymap.set
+
+keymap("n", "<C-Space>", "<cmd>WhichKey \\<space><cr>", opts)
+keymap("n", "<C-i>", "<C-i>", opts)
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Tabs --
+-- keymap("n", "<m-t>", ":tabnew %<cr>", opts)
+keymap("n", "<m-y>", ":tabclose<cr>", opts)
+keymap("n", "<m-\\>", ":tabonly<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Naviagate buffers
+keymap("n", "<TAB>", ":bnext<CR>", opts)
+keymap("n", "<S-TAB>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-q>", ":bdelete! %d<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Control keys
+keymap("n", "<C-s>", ":w<CR>", opts)
+keymap("n", "<C-q>", ":bd<CR>", opts)
+keymap("n", "<C-p>", ":Telescope find_files hidden=true prompt_prefix=🔍<CR>", opts) -- CTRL-P with telescope
+
+-- Remove highlight from search
+keymap("n", "<ESC><ESC>", ":nohlsearch<CR>", opts)
+
+-- Insert --
+-- Press jk or kj fast to enter
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+keymap("v", "p", '"_dP', opts)
+-- keymap("v", "P", '"_dP', opts)
+
+keymap("n", "Q", "<cmd>Bdelete!<CR>", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Command --
+-- Menu navigation
+keymap("c", "<C-j>", 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true })
+keymap("c", "<C-k>", 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true })
+
+keymap(
+  "n",
+  "<F6>",
+  [[:echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">" . " FG:" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"fg#")<CR>]],
+  opts
+)
+keymap("n", "<F7>", "<cmd>TSHighlightCapturesUnderCursor<cr>", opts)
+keymap("n", "<C-z>", "<cmd>ZenMode<cr>", opts)
+keymap("n", "-", ":lua require'lir.float'.toggle()<cr>", opts)
+keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
+keymap("n", "<m-v>", "<cmd>lua require('lsp_lines').toggle()<cr>", opts)
+
+keymap("x", "<m-/>", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', opts)
+keymap("n", "<m-/>", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
+
+vim.cmd [[
+  function! QuickFixToggle()
+    if empty(filter(getwininfo(), 'v:val.quickfix'))
+      copen
+    else
+      cclose
+    endif
+  endfunction
+]]
+
+keymap("n", "<m-q>", ":call QuickFixToggle()<cr>", opts)
+
+M.show_documentation = function()
+  local filetype = vim.bo.filetype
+  if vim.tbl_contains({ "vim", "help" }, filetype) then
+    vim.cmd("h " .. vim.fn.expand "<cword>")
+  elseif vim.tbl_contains({ "man" }, filetype) then
+    vim.cmd("Man " .. vim.fn.expand "<cword>")
+  elseif vim.fn.expand "%:t" == "Cargo.toml" then
+    require("crates").show_popup()
+  else
+    vim.lsp.buf.hover()
+  end
+end
+vim.api.nvim_set_keymap("n", "K", ":lua require('edward.keymaps').show_documentation()<CR>", opts)
+
+return M
---
+++ lua/edward/plugins.lua
@@ -0,0 +1,47 @@
+-- Additional Plugins
+lvim.plugins = {
+	"nvim-treesitter/playground",
+	"mfussenegger/nvim-jdtls",
+	"rcarriga/nvim-dap-ui",
+	"karb94/neoscroll.nvim",
+	"j-hui/fidget.nvim",
+	"windwp/nvim-ts-autotag",
+	"kylechui/nvim-surround",
+	"christianchiarulli/harpoon",
+	"MattesGroeger/vim-bookmarks",
+	"NvChad/nvim-colorizer.lua",
+	"ghillb/cybu.nvim",
+	"moll/vim-bbye",
+	"folke/todo-comments.nvim",
+	"windwp/nvim-spectre",
+	"is0n/jaq-nvim",
+	{
+		"0x100101/lab.nvim",
+		run = "cd js && npm ci",
+	},
+	"f-person/git-blame.nvim",
+	"ruifm/gitlinker.nvim",
+	"mattn/vim-gist",
+	"mattn/webapi-vim",
+	"folke/zen-mode.nvim",
+	"lvimuser/lsp-inlayhints.nvim",
+	"lunarvim/darkplus.nvim",
+	"lunarvim/templeos.nvim",
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "VimEnter" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup({
+					plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
+				})
+			end, 100)
+		end,
+	},
+
+	{ "zbirenbaum/copilot-cmp", after = { "copilot.lua", "nvim-cmp" } },
+
+	-- https://github.com/jose-elias-alvarez/typescript.nvim
+	-- "rmagatti/auto-session",
+	-- "rmagatti/session-lens"
+}
